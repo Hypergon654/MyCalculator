712f335ad6ae05b1b995a7747e0347c0804b09e7
lexus654 <72435812+lexus654@users.noreply.github.com>
2021-08-17 17:02:27 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
 "use strict";
-// let number1 = Number(document.querySelector(".firstValue").value);
-// let number2 = Number(document.querySelector(".secondValue").value);
-document.querySelector(".add").addEventListener("click", function () {
-  let number1 = Number(document.querySelector(".firstValue").value);
-  let number2 = Number(document.querySelector(".secondValue").value);
-  document.querySelector(".first").textContent = number1 + number2;
-  document.querySelector("body").style.backgroundColor = "#ECECEC";
+// this are the variables
+const one = document.querySelector(".key-1");
+const two = document.querySelector(".key-2");
+const three = document.querySelector(".key-3");
+const four = document.querySelector(".key-4");
+const five = document.querySelector(".key-5");
+const six = document.querySelector(".key-6");
+const seven = document.querySelector(".key-7");
+const eight = document.querySelector(".key-8");
+const nine = document.querySelector(".key-9");
+const dot = document.querySelector(".key-dot");
+const zero = document.querySelector(".key-0");
+const output = document.querySelector(".output");
+// Event listener
+one.addEventListener("click", function () {
+  const value = Number(one.textContent);
+  output.textContent = value;
 });
-document.querySelector(".sub").addEventListener("click", function () {
-  let number1 = Number(document.querySelector(".firstValue").value);
-  let number2 = Number(document.querySelector(".secondValue").value);
-  document.querySelector(".first").textContent = number1 - number2;
-  document.querySelector("body").style.backgroundColor = "#A2A2A2";
+two.addEventListener("click", function () {
+  const value = Number(two.textContent);
+  output.textContent = value;
 });
-document.querySelector(".mul").addEventListener("click", function () {
-  let number1 = Number(document.querySelector(".firstValue").value);
-  let number2 = Number(document.querySelector(".secondValue").value);
-  document.querySelector(".first").textContent = number1 * number2;
-  document.querySelector("body").style.backgroundColor = "#FFFFFF";
+three.addEventListener("click", function () {
+  value = Number(three.textContent);
+  output.textContent = value;
 });
-document.querySelector(".div").addEventListener("click", function () {
-  let number1 = Number(document.querySelector(".firstValue").value);
-  let number2 = Number(document.querySelector(".secondValue").value);
-  let qoutient = number1 / number2;
-  let final = qoutient.toFixed(2);
-  document.querySelector(".first").textContent = final;
-  document.querySelector("body").style.backgroundColor = "#7A7A79";
+four.addEventListener("click", function () {
+  value = Number(four.textContent);
+  output.textContent = value;
+});
+five.addEventListener("click", function () {
+  value = Number(five.textContent);
+});
+six.addEventListener("click", function () {
+  value = Number(six.textContent);
+});
+seven.addEventListener("click", function () {
+  value = Number(seven.textContent);
+});
+eight.addEventListener("click", function () {
+  value = Number(eight.textContent);
+});
+nine.addEventListener("click", function () {
+  value = Number(nine.textContent);
+});
+zero.addEventListener("click", function () {
+  value = Number(zero.textContent);
+});
+dot.addEventListener("click", function () {
+  value = dot.textContent;
 });
