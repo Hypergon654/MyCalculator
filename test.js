64a5ca2b876034cfe441fa976bbbe5d13612fd57
lexus654 <ronald.laz@tup.edu.ph>
2021-09-27 19:34:05 +0800

    Display Done

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const arr = ["1", "2", "3"];
+const arr = [];
 const arr2 = ["2", "5", "1"];
 // we can use the join operator to combine all the elements in the array
 // the use teh Number function to convert the string into a number
@@ -12,27 +12,28 @@ const btn2 = document.getElementById("2");
 const btn3 = document.getElementById("3");
 const btn4 = document.getElementById("4");
 const btn5 = document.getElementById("5");
+const display = document.getElementById("display");
 
 //TODO
 
 // Get the context of a Button
+
 const clickEvent = function (btn) {
   btn.addEventListener("click", function () {
     console.log(btn.textContent);
     const lastnumber = Number(btn.textContent);
     arr.push(lastnumber);
-    console.log(Number(arr.join("")));
+    display.textContent = Number(arr.join(""));
   });
 };
-
-clickEvent(btn1);
-clickEvent(btn2);
-clickEvent(btn3);
-clickEvent(btn4);
-clickEvent(btn5);
-
+console.log(arr);
 // Once Press append the value into an array  DONE
 
 // after pressing the operator the first array will combine all the elements
 
 // log the next value into the next array
+clickEvent(btn1);
+clickEvent(btn2);
+clickEvent(btn3);
+clickEvent(btn4);
+clickEvent(btn5);
