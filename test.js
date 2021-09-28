ab6cc74a899b6be7991c7d21b90c3e94cae537db
lexus654 <ronald.laz@tup.edu.ph>
2021-09-28 16:59:31 +0800

    quit

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const arr = [];
+var arr = [];
 const arr2 = ["2", "5", "1"];
 // we can use the join operator to combine all the elements in the array
 // the use teh Number function to convert the string into a number
@@ -12,20 +12,23 @@ const btn2 = document.getElementById("2");
 const btn3 = document.getElementById("3");
 const btn4 = document.getElementById("4");
 const btn5 = document.getElementById("5");
-const display = document.getElementById("display");
+const displayCurrent = document.getElementById("displayCurrent");
+const plus = document.getElementById("plus");
 
 //TODO
 
 // Get the context of a Button
 
 // Once Press append the value into an array  DONE
-
 const clickEvent = function (btn) {
   btn.addEventListener("click", function () {
     console.log(btn.textContent);
     const lastnumber = Number(btn.textContent);
     arr.push(lastnumber);
-    display.textContent = Number(arr.join(""));
+    displayCurrent.textContent = Number(arr.join(""));
+    if (true) {
+      plus.addEventListener("click", function () {});
+    }
   });
 };
 
@@ -35,8 +38,6 @@ const clickEvent = function (btn) {
 // same function with the first it will log into the display and join all the lements
 // once = is press it will compute the upper array and the curretn array
 // display the result
-
-console.log(arr);
 
 clickEvent(btn1);
 clickEvent(btn2);
